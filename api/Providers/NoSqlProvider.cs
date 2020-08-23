02b9b2adb2de97aed1794ea81596786b0b252ac0
Richard Tatum <richard.tatum@starkwood.co.uk>
2020-08-23 15:51:36 +0100

    Basic CRUD

---
+++ api/Providers/NoSqlProvider.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
+using api.Models;
+using Microsoft.Extensions.Options;
+
+namespace api.Providers
+{
+    public class NoSqlProvider
+    {
+        private readonly AmazonDynamoDBClient _client;
+        private readonly AwsConfig _config;
+        private readonly PutItemOperationConfig _putConfig;
+        private const string TableName = "ubisoft";
+
+        public NoSqlProvider(IOptions<AwsConfig> config)
+        {
+            _config = config.Value;
+            var credentials = new BasicAWSCredentials(_config.AccessKey, _config.SecretKey);
+            _client = new AmazonDynamoDBClient(credentials, RegionEndpoint.EUWest1);
+
+            _putConfig = new PutItemOperationConfig
+            {
+                ReturnValues = ReturnValues.UpdatedNewAttributes
+            };
+        }
+
+        public async Task EnsureProvisionedAsync(CancellationToken token)
+        {
+            try
+            {
+                await _client.DescribeTableAsync(TableName, token);
+                return;
+            }
+            catch (ResourceNotFoundException e)
+            {
+                // TODO: Log
+            }
+            // Default table information for the Ubisoft key table
+            var schema = new List<KeySchemaElement> { new KeySchemaElement("id", KeyType.HASH) };
+            var attributes = new List<AttributeDefinition> { new AttributeDefinition("id", ScalarAttributeType.S) };
+            var throughput = new ProvisionedThroughput(5, 5);
+
+            await _client.CreateTableAsync(TableName, schema, attributes, throughput, token);
+        }
+
+        public async Task<Document> GetItemAsync(string id)
+        {
+            if (!Table.TryLoadTable(_client, TableName, out var table ))
+                return null;
+
+            var response = await table.GetItemAsync(id);
+            return response;
+        }
+
+        public async Task<bool> DeleteItemAsync(string id, CancellationToken token)
+        {
+            if (!Table.TryLoadTable(_client, TableName, out var table))
+                return false;
+
+            var document = new Primitive(id, false);
+
+            var response = await table.DeleteItemAsync(document, token);
+            return true;
+        }
+
+        public async Task<Document> PutItemAsync(Ticket ticket, CancellationToken token)
+        {
+            await EnsureProvisionedAsync(token);
+
+            if (!Table.TryLoadTable(_client, TableName, out var table))
+                return null;
+
+            var attributes = new Dictionary<string, AttributeValue>
+            {
+                {"id", new AttributeValue(ticket.Id.ToString())},
+                {"value", new AttributeValue(ticket.Value)},
+                {"created", new AttributeValue(ticket.Created.ToString())}
+            };
+
+            var item = Document.FromAttributeMap(attributes);
+
+            var response = await table.PutItemAsync(item, _putConfig , token);
+            return response;
+        } 
+    }
+}
