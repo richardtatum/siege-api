8552401efaaae08ff51f48b6436c3c83741bec6c
Richard Tatum <richard.tatum@starkwood.co.uk>
2020-08-23 18:12:28 +0100

    Foundation for ticket provider

---
+++ api/Providers/TicketProvider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Models;
+
+namespace api.Providers
+{
+    public class TicketProvider
+    {
+        private readonly INoSqlProvider _provider;
+        private readonly CancellationTokenSource _source;
+        private readonly CancellationToken _token;
+
+        public TicketProvider(INoSqlProvider provider)
+        {
+            _provider = provider;
+            _source = new CancellationTokenSource();
+            _token = _source.Token;
+        }
+
+        public async Task<Ticket> GetTicket()
+        {
+            Ticket ticket;
+            try
+            {
+                ticket = await _provider.GetItemAsync<Ticket>("ticket", _token);
+            }
+            catch (Exception e)
+            {
+                _source.Cancel();
+                Console.WriteLine(e);
+                throw;
+            }
+
+            if (ticket == null) return null;
+
+            if (ticket.Valid) return ticket;
+            return null; // update ticket
+        }
+
+        private async Task<Ticket> UpdateTicket()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
